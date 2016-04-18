a36e25874763cff51d74800269b04e79819db4df
Eugene Cheipesh <echeipesh@gmail.com>
2016-04-18 12:14:34 -0400

    fix wait for accumulo script on cluster start

--- emr/start-cluster.sh
+++ emr/start-cluster.sh
@@ -14,4 +14,4 @@ aws emr create-cluster \
     Name=Master,InstanceCount=1,InstanceGroupType=MASTER,InstanceType=$MASTER_INSTANCE \
     Name=Workers,InstanceCount=$WORKER_COUNT,BidPrice=$WORKER_PRICE,InstanceGroupType=CORE,InstanceType=$WORKER_INSTANCE \
   --bootstrap-action Path=$EMR_TARGET/4/bootstrap-geowave.sh,Name=Bootstrap_GeoWave_Node \
-  --steps Type=CUSTOM_JAR,Name=Wait,Jar=s3://elasticmapreduce/libs/script-runner/script-runner.jar,Args=["s3:$EMR_TARGET/wait.sh"]
+  --steps Type=CUSTOM_JAR,Name=Wait,Jar=s3://elasticmapreduce/libs/script-runner/script-runner.jar,Args=["$EMR_TARGET/wait-for-accumulo.sh"]
