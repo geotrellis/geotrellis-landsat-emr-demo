bf3ef64f9455e82831b0a0e3a9d91d2f13b2dbfe
Eugene Cheipesh <echeipesh@gmail.com>
2016-04-20 09:55:23 -0400

    include query polygon

--- scripts/upload-code.sh
+++ scripts/upload-code.sh
@@ -8,6 +8,8 @@ aws s3 cp $DIR/emr/bootstrap-geowave.sh $EMR_TARGET/
 aws s3 cp $DIR/emr/geowave-install-lib.sh $EMR_TARGET/
 aws s3 cp $DIR/emr/wait-for-accumulo.sh $EMR_TARGET/
 aws s3 cp $DIR/emr/tile-server.sh $EMR_TARGET/
+aws s3 cp $DIR/polygon.json $EMR_TARGET/
+
 # upload ingest assembly
 aws s3 cp $DIR/../ingest/target/scala-2.10/ingest-assembly-0.1.0.jar $EMR_TARGET/
 aws s3 cp $DIR/../server/target/scala-2.10/server-assembly-0.1.0.jar $EMR_TARGET/
