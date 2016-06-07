055f4ef85d50f853a935858a8ad3ee6804891e25
Eugene Cheipesh <echeipesh@gmail.com>
2016-06-07 15:35:55 -0400

    add start/end date params

--- scripts/start-ingest.sh
+++ scripts/start-ingest.sh
@@ -4,19 +4,23 @@ for i in "$@"
 do
 case $i in
     --cluster-id=*)
-    CLUSTER_ID="${i#*=}"
-    shift
-    ;;
+        CLUSTER_ID="${i#*=}"
+        shift;;
     --bbox=*)
-    BBOX="${i#*=}"
-    shift
-    ;;
+        BBOX="${i#*=}"
+        shift;;
+    --start-date=*)
+        START_DATE="${i#*=}"
+        shift;;
+    --end-date=*)
+        END_DATE="${i#*=}"
+        shift;;
 esac
 done
 
-if [[ -z $CLUSTER_ID || -z $BBOX ]]
+if [[ -z $CLUSTER_ID || -z $BBOX || -z $START_DATE || -z $END_DATE]]
 then
-  echo "usage: start-ingest --cluster-id=<CLUSTER_ID> --BBOX=<xmin,ymin,xmax,ymax>"
+  echo "usage: start-ingest --cluster-id=<CLUSTER_ID> --BBOX=<xmin,ymin,xmax,ymax> --start-date=yyyy-mm-dd --end-date=yyyy-mm-dd"
   exit
 fi
 
@@ -35,8 +39,8 @@ ARGS="spark-submit,--master,yarn-cluster,\
 $EMR_TARGET/ingest-assembly-0.1.0.jar,\
 --layerName,landsat,\
 --bbox,\"$BBOX\",\
---startDate,2015-08-01,\
---endDate,2016-01-01,\
+--startDate,$START_DATE,\
+--endDate,$END_DATE,\
 --output,accumulo,\
 --params,\"instance=accumulo,table=tiles,user=root,password=secret\""
 
